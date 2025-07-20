e6370d87a0318e05dfa4bac4410d60b687503bc0
prakritish <bhattacharyaprakritish@gmail.com>
2025-07-21 01:59:19 +0530

    Initial commit: Flask basic app

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<h1>Hey Prakritish, welcome from Flask end</h1>"
+
+@app.route("/<name>")
+def hello(name):
+    return f"<h1>Hello, {escape(name)}</h1>"
