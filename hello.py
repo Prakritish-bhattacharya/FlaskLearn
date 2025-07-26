86efd17826ebef16eae3b9faba2b3c30186e841b
prakritish <bhattacharyaprakritish@gmail.com>
2025-07-27 01:33:55 +0530

    POST method completed...

--- hello.py
+++ hello.py
@@ -1,22 +1,21 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, abort, render_template, request, redirect, flash, url_for
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Needed for flash to work
 
 
-@app.route('/')
-def registration():
-    # return render_template('formfetch.html')
-    return render_template('formfile.html')
+@app.route('/form', methods =['POST','GET'])
+def handle_form():
+    if request.method == 'POST':
+        # Get the form data
+        name = request.form['name']
+        email = request.form['email']
+        return redirect(url_for('success',  name = name , email = email))
+    return render_template('form.html')
 
-@app.route('/success', methods = ['POST'])
-def printdata():
-    result = request.form
-    return render_template('success.html', result = result)
-
-@app.route('/filesubmit', methods = ['POST'])
-def filesubmit():
-    f = request.files['file']
-    f.save('static/'+f.filename)
-    return "success"
-    # return re
+@app.route('/success')
+def success():
+    
+    name = request.args.get('name')
+    email = request.args.get('email')
+    return render_template('success.html', name = name, email = email)
