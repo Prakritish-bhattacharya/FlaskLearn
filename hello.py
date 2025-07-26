788b89e500865ea8284fa828ba69f51259a205b2
prakritish <bhattacharyaprakritish@gmail.com>
2025-07-27 03:16:07 +0530

    POST method completed...

--- hello.py
+++ hello.py
@@ -4,18 +4,10 @@ app = Flask(__name__)
 # use secret key for flashing
 app.secret_key = "I love you"
 
-@app.route('/form', methods =['POST','GET'])
-def handle_form():
-    if request.method == 'POST':
-        # Get the form data
-        name = request.form['name']
-        email = request.form['email']
-        return redirect(url_for('success',  name = name , email = email))
-    return render_template('form.html')
+@app.route('/')
+def index():
+    return render_template('home.html')
 
-@app.route('/success')
-def success():
-    
-    name = request.args.get('name')
-    email = request.args.get('email')
-    return render_template('success.html', name = name, email = email)
+@app.route('/about')
+def about():
+    return render_template('about.html')
