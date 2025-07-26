f168b5b186f99e18d05a5226cb5501e5f9a2d0f1
prakritish <bhattacharyaprakritish@gmail.com>
2025-07-27 00:02:19 +0530

    POST method completed...

--- hello.py
+++ hello.py
@@ -6,10 +6,17 @@ app.secret_key = 'your_secret_key'  # Needed for flash to work
 
 @app.route('/')
 def registration():
-    return render_template('formfetch.html')
+    # return render_template('formfetch.html')
+    return render_template('formfile.html')
 
 @app.route('/success', methods = ['POST'])
 def printdata():
     result = request.form
-    
     return render_template('success.html', result = result)
+
+@app.route('/filesubmit', methods = ['POST'])
+def filesubmit():
+    f = request.files['file']
+    f.save('static/'+f.filename)
+    return "success"
+    # return re
