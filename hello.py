860c158d968d92622fbb314f7dd6328dec8af3c6
prakritish <bhattacharyaprakritish@gmail.com>
2025-08-07 15:26:56 +0530

    wtForm completed...

--- hello.py
+++ hello.py
@@ -1,44 +1,56 @@
-import dbm
 from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email, ValidationError
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import EmailField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Regexp, Email
 
+# import form  # we imported 4 required functions from wtforms.validators for wtforms validation
 
-    
 app = Flask(__name__)
-app.secret_key = "osdfbn;"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.secret_key = "fhv"  # here we need secret_key for CSRF token
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mytry.db'
 db = SQLAlchemy(app)
 
-#  create dataBase table
-class FormData(db.Model): # type: ignore
+# .................create database table..................
+class FormData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-
+    phone = db.Column(db.String(10), nullable = False, unique = True)
+# use app_context for create the table
 with app.app_context():
     db.create_all()
-#  Fileds , How meny fileds you are required
-class MyForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()] )
+
+#  ....................create required form.....................
+class MyForm(FlaskForm):  #pass FlaskForm for flask form validation
+    name = StringField('Name :', validators=[DataRequired()]) 
+    email = EmailField('Email', validators= [DataRequired(), Email()])
+    phone = StringField('Phone No :', validators= [
+        DataRequired( message = "phone number is required."),
+        Length(min = 10, max = 11, message="Phone number must be between 10 and 15 digits."),
+        Regexp(r'^\+?[0-9]{10,15}$', message="Enter a valid phone number.")
+        ])
     submit = SubmitField('Submit')
 
-@app.route('/', methods = ['GET','POST'])
+#  create Route for form
+@app.route('/wtform', methods = ['GET','POST'])
 def index():
-    form = MyForm()
-    #  write HTTP methods while form uploading
-    if request.method == "POST" and form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        # save into database
-        form_data = FormData(name = name, email = email)
+    FrontendForm = MyForm()
+    
+    #  check HTTP methods while form are submitted
+    if request.method == "POST" and FrontendForm.validate_on_submit():
+        name = FrontendForm.name.data  # take all data from form field
+        email = FrontendForm.email.data
+        phone = FrontendForm.phone.data
+        
+        # save data into db
+        form_data = FormData(name = name , email = email, phone =  phone)
         db.session.add(form_data)
         db.session.commit()
         return "success"
-    return render_template('index.html', form=form)
-if __name__ == '__main__':
-    app.run(debug=True)
+        
+    return render_template('index.html', sendToFrontedForm = FrontendForm)
     
+    
+if __name__ == '__main__':
+    app.run( debug = True )
