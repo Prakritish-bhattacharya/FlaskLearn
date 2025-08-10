259080a72511e6477d4c64e50d3759a496a40bdb
prakritish <bhattacharyaprakritish@gmail.com>
2025-08-10 21:06:57 +0530

    CRUD Functions...

--- hello.py
+++ hello.py
@@ -71,7 +71,7 @@ def index():
 @app.route('/formdata')
 def formData():
     formDt = FormData.query.all()
-    return render_template('formdata.htmlg', formDt = formDt)
+    return render_template('formdata.html', formDt = formDt)
 
 @app.route('/users/<int:id>/edit', methods = ['GET','POST'])
 def edit(id):
