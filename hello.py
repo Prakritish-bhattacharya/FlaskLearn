b97f2feb7723ae148ce76159ab71fc2d2de04878
prakritish <bhattacharyaprakritish@gmail.com>
2025-08-15 09:13:06 +0530

    wtForm completed...

--- hello.py
+++ hello.py
@@ -93,7 +93,7 @@ def index():
 @app.route('/formdata')
 def formData():
     formDt = FormData.query.all()
-    return render_template('formdata.html1234567859', formDt = formDt )
+    return render_template('formdata.html123456859', formDt = formDt )
 
 @app.route('/users/<int:id>/edit', methods = ['GET','POST'])
 def edit(id):
